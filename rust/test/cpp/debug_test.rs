bd760560441dcec1c59f86f4ca75dc2e8aecb01b
Jakob Buchgraber <buchgr@google.com>
2024-06-04 02:53:10 -0700

    Fix debug_test to work with C++ lite
    This change adds a cfg attribute 'cpp_lite' to the C++ kernel of Protobuf Rust. If C++ lite is selected on the command line, the cfg attribute 'cpp_lite' is set. The root cause of the test failure was that the Debug implementation for full C++ protos uses  text proto which is not available in C++ lite. The fix uses the 'cpp_lite' cfg attribute to select a different Debug implementation that doesn't rely on text proto
    
    PiperOrigin-RevId: 640085104

--- rust/test/cpp/debug_test.rs
+++ rust/test/cpp/debug_test.rs
@@ -1,6 +1,7 @@
 use debug_rust_proto::DebugMsg;
 use googletest::prelude::*;
 
+#[cfg(not(lite_runtime))]
 #[test]
 fn test_debug() {
     let mut msg = DebugMsg::new();
@@ -9,4 +10,14 @@ fn test_debug() {
 
     assert_that!(format!("{msg:?}"), contains_substring("id: 1"));
     assert_that!(format!("{msg:?}"), not(contains_substring("password")));
+}
+
+#[cfg(lite_runtime)]
+#[test]
+fn test_debug_lite() {
+    let mut msg = DebugMsg::new();
+    msg.set_id(1);
+    msg.set_secret_user_data("password");
+
+    assert_that!(format!("{msg:?}"), contains_substring("MessageLite"));
 }
